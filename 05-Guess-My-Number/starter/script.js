73711043793814f7f5e1e2925e191ad7f4b26429
Anthony Cordero <tony170593@gmail.com>
2025-09-09 21:19:06 -0600

    //  JS CHALLENGE # 1!
    /*
              INSTRUCTIONS
    
    Implement a game rest functionality, so rgar the player cab make anew guess!
    
    1. Select the element with the 'againg' class and attach a click event handler
    2. In the handler function, restore a initil values of the score and number variables
    3. Restore the inicial conditions ofthe message, number, score and guess input field
    4. Also restore the original background colo (#222) and number width (15rem)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,7 +11,7 @@
 // console.log(document.querySelector('.guess').value);
 
 // Generate a random secret number between 1 and 20
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 // Set the initial score
 let score = 20;
@@ -19,7 +19,7 @@ let score = 20;
 // Add an event listener to the "Check!" button
 document.querySelector('.check').addEventListener('click', function () {
   // Get the user's guess from the input and convert it to a number
-  const guess = Number(document.querySelector('.guess').value);
+  let guess = Number(document.querySelector('.guess').value);
 
   // When there's no input (falsy value like 0, '', null)
   if (!guess) {
@@ -74,6 +74,27 @@ Implement a game rest functionality, so rgar the player cab make anew guess!
 1. Select the element with the 'againg' class and attach a click event handler
 2. In the handler function, restore a initil values of the score and number variables
 3. Restore the inicial conditions ofthe message, number, score and guess input field
-4. Also restore the original background colo (#222) abd number width (15rem)
+4. Also restore the original background colo (#222) and number width (15rem)
 
 */
+// Select the element with the 'againg' class and attach a click event handler
+
+document.querySelector('.again').addEventListener('click', function () {
+  //In the handler function, restore a initil values of the score and number variables
+
+  // Reset SecretNumber
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // Reset Score
+  document.querySelector('.score').textContent = '20';
+
+  // Restore the inicial conditions ofthe message, number, score and guess input field
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  // Restore the original background color, and number width
+  // Reset background color
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
